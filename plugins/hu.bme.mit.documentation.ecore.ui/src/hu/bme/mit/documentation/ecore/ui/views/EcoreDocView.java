9dc24e68476d78074a19bda4f61e92bd9bf30bc1
Istvan Rath <rath@mit.bme.hu>
2013-05-20 15:24:15 +0200

    support for the creation of documentation fields that are not existing yet.

--- plugins/hu.bme.mit.documentation.ecore.ui/src/hu/bme/mit/documentation/ecore/ui/views/EcoreDocView.java
+++ plugins/hu.bme.mit.documentation.ecore.ui/src/hu/bme/mit/documentation/ecore/ui/views/EcoreDocView.java
@@ -6,6 +6,7 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.ecore.EAnnotation;
 import org.eclipse.emf.ecore.ENamedElement;
+import org.eclipse.emf.ecore.EcoreFactory;
 import org.eclipse.incquery.runtime.api.IncQueryEngine;
 import org.eclipse.incquery.runtime.exception.IncQueryException;
 import org.eclipse.jface.resource.JFaceColors;
@@ -15,6 +16,8 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Text;
@@ -29,23 +32,23 @@ import ecoredocgen.incquery.ECoreDocumentationMatcher;
 
 public class EcoreDocView extends ViewPart {
 
-	Text text;
-	ENamedElement currentElement;
-	EAnnotation currentAnnotation;
+	private Text text;
+	private ENamedElement currentElement;
+	private EAnnotation currentAnnotation;
 	
 	private void clearCurrentState() {
 		text.setText("");
 		currentElement=null;
 		currentAnnotation=null;
-		text.setEnabled(false);
+		//text.setEnabled(false); // not a good idea as this disables mouse listeners
 		text.setBackground(JFaceColors.getErrorBackground(Display.getCurrent()));
 	}
 	
-	private void setCurrentState(ECoreDocumentationMatch m) {
-		text.setEnabled(true);
-		text.setText(m.getDoc());
-		this.currentElement = m.getHost();
-		this.currentAnnotation = m.getAnn();
+	private void setCurrentState(String textContents, ENamedElement currE, EAnnotation currA) {
+		//text.setEnabled(true); // not a good idea as this disables mouse listeners
+		text.setText(textContents);
+		this.currentElement = currE;
+		this.currentAnnotation = currA;
 		text.setBackground(JFaceColors.getBannerBackground(Display.getCurrent()));
 	}
 	
@@ -61,7 +64,7 @@ public class EcoreDocView extends ViewPart {
 					IncQueryEngine engine = IncQueryEngine.on(target.eResource());
 					ECoreDocumentationMatcher matcher = ECoreDocumentationMatcher.on(engine);
 					for (ECoreDocumentationMatch m :matcher.getAllMatches(target, null, null)) {
-						setCurrentState(m);
+						setCurrentState(m.getDoc(), m.getHost(), m.getAnn());
 					}
 				} catch (IncQueryException e) {
 					Activator.getDefault().getLog().log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, e.getMessage()));
@@ -80,8 +83,6 @@ public class EcoreDocView extends ViewPart {
 			}
 		});
 	}
-
-	
 	
 	@Override
 	public void createPartControl(Composite parent) {
@@ -89,30 +90,55 @@ public class EcoreDocView extends ViewPart {
 		text.setFont(JFaceResources.getTextFont());
 				
 		text.addKeyListener(new KeyListener() {
-			
 			@Override
 			public void keyReleased(KeyEvent e) {
-				if (currentAnnotation!=null) {
+				if (currentElement!=null && currentAnnotation!=null) {
 					if (!currentAnnotation.getDetails().get("documentation").equals(text.getText())) {
-						currentAnnotation.getDetails().put("documentation", text.getText());
-						// TODO this should be done through a TransactionalEditingDomain command
-						// in order to trigger the dirty flag in the host editor
+						updateDocContentsInModel();
 					}
+				}
+			}
+			@Override
+			public void keyPressed(KeyEvent e) { }
+		});
+		text.addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseUp(MouseEvent e) {
+				if (currentElement!=null && currentAnnotation==null) {
+					// the element doesn't yet have a documentation annotation
+					// so let's create one
+					setCurrentState(newDocumentation, currentElement, createNewDocFieldInModel());
 				}
 			}
 			
 			@Override
-			public void keyPressed(KeyEvent e) {
-				
-			}
+			public void mouseDown(MouseEvent e) {}
+			
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {}
 		});
-		// TODO add support for the case when the element doesn't yet have a documentation annotation
-		// which is probably indicated by the condition (currentElement: notNull AND currentAnnotation: null)
 	}
-
+	
 	@Override
-	public void setFocus() {
-		updateStateAccordingToSelection(getSite().getWorkbenchWindow().getSelectionService().getSelection());
-	}
+	public void setFocus() { }
 
+	//////////// model manipulation ///////////////
+	
+	// TODO these operations should be done through a TransactionalEditingDomain command
+	// in order to trigger the dirty flag in the host editor
+	
+	private void updateDocContentsInModel() {
+		currentAnnotation.getDetails().put("documentation", text.getText());
+	}
+	
+	private static String newDocumentation = "Please write some documentation here.";
+	
+	private EAnnotation createNewDocFieldInModel() {
+		EAnnotation newAnn = EcoreFactory.eINSTANCE.createEAnnotation();
+		newAnn.setSource("http://www.eclipse.org/emf/2002/GenModel");
+		newAnn.getDetails().put("documentation", ""); // create intentionally as empty, so that actual model contents are not cluttered
+		currentElement.getEAnnotations().add(newAnn);
+		return newAnn;
+	}
 }
